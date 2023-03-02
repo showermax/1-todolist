2434ed20a475be1730cf9cfd28a8aece65fd524b
showermax <sevenhauz@gmail.com>
2023-03-02 17:22:00 +0300

    first fiter

--- src/App.tsx
+++ src/App.tsx
@@ -16,19 +16,26 @@ function App(): JSX.Element {
         {id: 4, title: "ES6/TS", isDone: false},
     ]
     )
-    const arrofTasks2: TaskType[] = [
-        {id: 1, title: "HTML&CSS", isDone: false},
-        {id: 2, title: "CSS&SCSS", isDone: false},
-        {id: 3, title: "ES6/TS", isDone: false},
-    ]
+
+    // const arrofTasks2: TaskType[] = [
+    //     {id: 1, title: "HTML&CSS", isDone: false},
+    //     {id: 2, title: "CSS&SCSS", isDone: false},
+    //     {id: 3, title: "ES6/TS", isDone: false},
+    // ]
     // let [stateTasks, setStateTasks] = useState(arrofTasks)
     function removeTask(id:number) {
         let newTasks = arrofTasks.filter((el) => el.id != id)
         setStateTasks(newTasks)
     }
+    function filterTasks(f: string) {
+        if (f=="Active") {
+            let newTasks = arrofTasks.filter((el) => el.isDone == true)
+            setStateTasks(newTasks)
+        }
+    }
     return (
         <div className="App">
-        <Todolist title = {"What to do"} tasks = {arrofTasks} classCss={"to-do"} remove={removeTask}/>
+        <Todolist title = {"What to do"} tasks = {arrofTasks} classCss={"to-do"} remove={removeTask} filter={filterTasks}/>
         {/*<Todolist title = {"In progress"} tasks = {arrofTasks2} classCss={"in-progress"}/>*/}
         </div>
     );
