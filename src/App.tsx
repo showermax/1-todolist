5383c4d4e34cbc4608bdec1e15e5d2db35a5b8c1
showermax <sevenhauz@gmail.com>
2023-03-02 17:10:35 +0300

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,27 @@ export type TaskType = {
     isDone: boolean
 }
 function App(): JSX.Element {
-    const arrofTasks: TaskType[] = [
+    let [arrofTasks, setStateTasks] = useState<TaskType[]>(
+        [
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "CSS&SCSS", isDone: true},
         {id: 3, title: "ES6/TS", isDone: false},
         {id: 4, title: "ES6/TS", isDone: false},
     ]
+    )
     const arrofTasks2: TaskType[] = [
         {id: 1, title: "HTML&CSS", isDone: false},
         {id: 2, title: "CSS&SCSS", isDone: false},
         {id: 3, title: "ES6/TS", isDone: false},
     ]
-    let [stateTasks, setStateTasks] = useState(arrofTasks)
+    // let [stateTasks, setStateTasks] = useState(arrofTasks)
     function removeTask(id:number) {
-        let newTasks = stateTasks.filter((el) => el.id != id)
+        let newTasks = arrofTasks.filter((el) => el.id != id)
         setStateTasks(newTasks)
     }
     return (
         <div className="App">
-        <Todolist title = {"What to do"} tasks = {stateTasks} classCss={"to-do"} remove={removeTask}/>
+        <Todolist title = {"What to do"} tasks = {arrofTasks} classCss={"to-do"} remove={removeTask}/>
         {/*<Todolist title = {"In progress"} tasks = {arrofTasks2} classCss={"in-progress"}/>*/}
         </div>
     );
